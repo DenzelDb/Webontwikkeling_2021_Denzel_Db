f6873b94dc1176da71ad03f5ed0142af3089a029
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-03-03 13:29:00 +0100

    oefening1 zonder funtion

--- Labo3/oefening1.ts
+++ Labo3/oefening1.ts
@@ -1,6 +1,6 @@
-let items: Array<number> = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87]
+let items = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87]
 
-function search (items, n){
+const search = (items: number[], n: number):number => {
     return items.indexOf(n);
 }
 console.log(search(items, 18));
