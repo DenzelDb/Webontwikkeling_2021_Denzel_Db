d827c996fb34a90061248ded5b9f9047a64cd4b8
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-03-24 14:53:06 +0100

    oefening2 zf

--- Labo6/oefening2.ts
+++ Labo6/oefening2.ts
@@ -1,3 +1,9 @@
 let names = ['joske','franske','donald','achmed']
 let capitalNames1 = names.map(name => name.toUpperCase());
+
+//let capitalNames2 = names.forEach(name => name.toUpperCase());
+let capitalNames2: string[] = [];
+names.forEach(name => capitalNames2.push(name.toUpperCase()))
+
 console.log(capitalNames1)
+console.log(capitalNames2)
