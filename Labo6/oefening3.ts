bc4e4c1e0c3414c9a6524ea15afc6ba50d83d5c4
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-03-31 13:17:46 +0200

    labo 7

--- Labo6/oefening3.ts
+++ Labo6/oefening3.ts
@@ -20,3 +20,46 @@ console.log(starters)
 
 let names: string[] = starters.map(e => e.name);
 console.log(names)
+
+let weakPokemon : Pokemon[] = starters.filter((pokemon: Pokemon) => {
+    return (pokemon.xp < 40)
+})
+console.log(weakPokemon)
+
+let weakPokemonNames : string[] = starters
+.filter(pokemon => pokemon.xp < 40)
+.map(pokemon => pokemon.name)
+
+console.log(weakPokemonNames)
+
+let sumOfAllXp : number = starters.reduce((sum: number, pokemon: Pokemon) =>{
+    return sum + pokemon.xp
+},0)
+
+console.log(sumOfAllXp)
+
+let strongestPokemon : Pokemon = starters.reduce((currentStrongestPokemon: Pokemon, currentPokemon: Pokemon) => {
+    if (currentPokemon.xp >= currentStrongestPokemon.xp){
+        return currentPokemon
+    }
+    else{
+        return currentStrongestPokemon;
+    }
+})
+
+let sortedStarters = starters.sort((a: Pokemon, b: Pokemon) => {
+    if (a.xp === b.xp){
+        if (a.name > b.name){
+            return 1
+        }
+        else{
+            return -1
+        }
+    }
+    if (a.xp > b.xp){
+        return 1;
+    }
+    else {
+        return -1;
+    }
+})
