80461e81ccb0d9644a9c1a3ec3db7b01d1c15ef3
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-04-21 15:31:20 +0200

    l7 oef3

---
+++ Labo7/oefening3.ts
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const showAllDadJokes = async() => {
+    for (let i=1;i<3;i++){
+        let jokes = await fetch(`https://icanhazdadjoke.com/search?term=dog&limit=5&page=${i}`, {
+            headers: {
+              'Accept': 'application/json'
+            }
+         })
+        let jokesJson = await jokes.json();
+        for (let d=0; d<5; d++){
+            console.log(jokesJson.results[d].joke)
+        }
+    }
+}
+
+showAllDadJokes();
+
+export{};
