7edba2d1f7d2542079f323d88d444778335ab3c6
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-05-12 15:18:05 +0200

    labo 10.2

--- Labo10/index.ts
+++ Labo10/index.ts
@@ -6,25 +6,29 @@ const {MongoClient} = require('mongodb');
 const uri = "mongodb+srv://DenzelDb:Webontwikkeling@webontwikkeling-ddb.jyws9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
+
+interface Movies{
+    name: string,
+    myScore: number,
+    timesViewed: number
+}
+let movies: Movies[] = [
+    {name: "The Matrix", myScore: 90, timesViewed: 10},
+    {name: "Pulp Fiction", myScore: 100, timesViewed: 100},
+    {name: "Monster Hunter", myScore: 5, timesViewed:1},
+    {name: "Blade Runner", myScore: 100, timesViewed:30},
+    {name: "Austin Powers", myScore: 80, timesViewed:10},
+    {name: "Jurasic Park 2", myScore: 40, timesViewed:1},
+    {name: "Ichi the Killer", myScore: 80, timesViewed:1}
+];
+
 const main = async() => {
     try {
         await client.connect();
         let list = await client.db().admin().listDatabases();
         console.log(list)
-        interface Movies{
-            name: string,
-            myScore: number,
-            timesViewed: number
-        }
-        let movies: Movies[] = [
-            {name: "The Matrix", myScore: 90, timesViewed: 10},
-            {name: "Pulp Fuction", myScore: 100, timesViewed: 100},
-            {name: "Monster Hunter", myScore: 5, timesViewed:1},
-            {name: "Blade Runner", myScore: 100, timesViewed:30},
-            {name: "Austin Powers", myScore: 80, timesViewed:10},
-            {name: "Jurasic Park 2", myScore: 40, timesViewed:1},
-            {name: "Ichi the Killer", myScore: 80, timesViewed:1}
-        ];
+        await client.db('WebOntwikkeling').collection('Movies').deleteMany({});
+
         await client.db('WebOntwikkeling').collection('Movies').insertMany(movies);
     } 
     catch (e){
