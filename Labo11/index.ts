01734c0cc80bbaaffd57eebe4ed3a4d60f092a9d
Denzel De bruyn <denzel.debruyn@student.ap.be>
2021-05-28 19:00:51 +0200

    labo 11 compleet

--- Labo11/index.ts
+++ Labo11/index.ts
@@ -5,9 +5,11 @@ const {MongoClient, ObjectId} = require('mongodb');
 const uri = "mongodb+srv://DenzelDb:Webontwikkeling@webontwikkeling-ddb.jyws9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended:true}));
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'),
 app.set('port', 3000);
 
 interface Movies{
@@ -36,11 +38,15 @@ app.get('/',(req:any,res:any)=>{
     res.render('landing')
 });
 
+const renderAllMovies = async (res:any) => {
+    let cursor = await client.db("WebOntwikkeling").collection("Movies").find({});
+    let allMovies = await cursor.toArray();
+    res.render('movies', {allMovies});
+}
+
 app.get('/movies',async (req:any,res:any)=>{
     try {
-        let cursor = await client.db("WebOntwikkeling").collection("Movies").find({});
-        let allMovies = await cursor.toArray();
-        res.render('movies', {allMovies});
+        renderAllMovies(res);
     } catch (e) {
         console.log(e)
     }
@@ -54,7 +60,34 @@ app.get('/movies/:x', async(req:any,res:any)=>{
     } catch (e) {
         console.log(e)
     }
+});
 
+app.get('/removemovie/:x', async(req:any,res:any)=>{
+    try {
+        let x = req.params.x;
+        let movie = await client.db("WebOntwikkeling").collection("Movies").removeOne({_id:ObjectId(x)});
+        renderAllMovies(res)
+    } catch (e) {
+        console.log(e)
+    }
+});
+
+app.get('/addmovie',(req:any,res:any)=>{
+    res.render("addmovie", {info:undefined})
+});
+
+app.post('/addmovie',async (req:any,res:any)=>{
+    try {
+        let newMovie:Movies = {
+            name: req.body.title,
+            myScore: req.body.score,
+            Director: req.body.director
+        }
+        await client.db("WebOntwikkeling").collection("Movies").insertOne(newMovie);
+        renderAllMovies(res);
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 export{};
